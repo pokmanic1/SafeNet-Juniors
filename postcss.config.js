1376a46602b35d53ac2fda195a208977651ff67f
pokmanic1 <fanulluimadalin13@gmail.com>
2026-02-10 09:49:54 +0300

    am creat tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,6 @@
 module.exports = {
-  plugins: {
-    // Tailwind configurat inline (nu trebuie tailwind.config.js separat)
-    tailwindcss: {
-      content: [
-        "./index.html",
-        "./public/**/*.html",
-        "./src/**/*.{html,js}"
-      ],
-      theme: {
-        extend: {
-          colors: {
-            primary: "#2563eb",
-            secondary: "#16a34a",
-            accent: "#f59e0b",
-          }
-        },
-      },
-      plugins: [],
-    },
-    autoprefixer: {},
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 }
