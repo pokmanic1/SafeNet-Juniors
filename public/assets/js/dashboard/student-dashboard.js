189b082d785a059dbd7b4970ea3b5192c912f7cb
pokmanic1 <fanulluimadalin13@gmail.com>
2026-02-17 14:41:41 +0300

    adaugare la pagina despre noi si blocarea paginilor de autentificare si inregistrare odata logat

--- public/assets/js/dashboard/student-dashboard.js
+++ public/assets/js/dashboard/student-dashboard.js
@@ -1,10 +1,16 @@
 import { auth, onAuthStateChanged } from "../fierbase/firebase-init.js";
-
-
+console.log(auth)
+const x = document.getElementById("ConecteazataID");
+const y = document.getElementById("InregistreazataID");
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
-
+        x.innerHTML = `
+        <div class="flex items-center">
+            <p  class="w-[60px] text-[13px] ">Conectata  ${user.username}</p>
+            <div class="ml-3 w-8 h-8 rounded-[40px] bg-black"></div>
+        </div>`;
+        y.style.display = "none";
         console.log(`Bună ziua, ${user.email}`)
     } else {
         console.log('conecteazate')
