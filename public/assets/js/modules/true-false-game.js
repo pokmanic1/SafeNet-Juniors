fad4ed8bf3e22457bffb18127185ecd1df6e9e8c
pokmanic1 <fanulluimadalin13@gmail.com>
2026-02-19 00:23:39 +0300

    crearea designului jocul true false -si verficarea indicilor din arey ca sa nu se repete propozitiile

--- public/assets/js/modules/true-false-game.js
+++ public/assets/js/modules/true-false-game.js
@@ -55,29 +55,111 @@ let arr = [
             { text: 'w 3', raspuns: true }
         ]
     },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    },
+    {
+        propozitii: [
+            { text: 'x 1', raspuns: true },
+            { text: 'y 2', raspuns: false },
+            { text: 'w 3', raspuns: true }
+        ]
+    }
 ]
 let newArr = [];
-for (let i = 0; i < 10; i++) {
-    let nr;
-    let adv = true;
-    while (adv) {
-        nr = Math.floor(Math.random() * 100);
-        if (nr < arr.length) {
-            adv = false;
-        }
-    }
-    for (let j = 0; j < arr.length; j++) {
-        if (nr === j) {
-            newArr.push(arr[j]);
-        }
-    }
-
-}
-
 let arrCuExercitii = [];
-for (let i = 0; i < newArr.length; i++) {
-    let nr = Math.floor(Math.random() * 3);
-    arrCuExercitii.push(newArr[i].propozitii[nr]);
+let arrIndex = [];
+creareArr();
+
+function creareArr() {
+    for (let i = 0; i < 10; i++) {
+        let nr;
+        let adv = true;
+        while (adv) {
+            let mem =false;
+            nr = Math.floor(Math.random() *  arr.length);
+            for (let j = 0; j < arrIndex.length; j++) {
+                if(nr===arrIndex[j]){
+                    mem=true;
+                }
+            }
+                if ( !mem) {
+                    adv = false;
+                    console.log(nr);arrIndex.push(nr);
+                }
+                
+
+        }
+        for (let j = 0; j < arr.length; j++) {
+            if (nr === j) {
+                newArr.push(arr[j]);
+
+            }
+        }
+
+    }
+
+
+    for (let i = 0; i < newArr.length; i++) {
+        let nr = Math.floor(Math.random() * 3);
+        arrCuExercitii.push(newArr[i].propozitii[nr]);
+
+        console.log('arraiul');
+        console.log(newArr[i].propozitii[nr]);
+    }
 }
 
 
@@ -87,21 +169,42 @@ for (let i = 0; i < newArr.length; i++) {
 let contor = 0;
 let contorCorect = 0;
 function genereazaHTML() {
-    if (contor < 10)    {
-    let nrIntrebare = `Intrebare ${contor +1}`;
-     let intrebare = arrCuExercitii[contor].text;
-    
-    document.querySelector('.nrIntrebare').innerHTML=`Intrebare ${contor +1}`;
-    document.querySelector('.textIntrebare').innerHTML=`${arrCuExercitii[contor].text}`;
+
+    if (contor < 10) {
+
+        document.querySelector('.restart').innerHTML = "";
+
+        document.querySelector('#true').style.display = 'inline-block';
+        document.querySelector('#false').style.display = 'inline-block';
+
+        document.querySelector('.nrIntrebare').innerHTML = `Intrebare ${contor + 1}`;
+        document.querySelector('.textIntrebare').innerHTML = `${arrCuExercitii[contor].text}`;
+        document.querySelector(".scorul").innerHTML = `Scorul ${contorCorect}`;
+        document.querySelector(".scorul").style.display = 'block';
+
     }
     else {
-        document.querySelector('.nrIntrebare').innerHTML=`Felicitări! Ai terminat jocul!`;
-        document.querySelector('.textIntrebare').innerHTML=`Ai răspuns corect la ${contorCorect} întrebări din 10.`;
+
+        document.querySelector(".scorul").innerHTML = `Scorul ${contorCorect}`;
+        document.querySelector('.textIntrebare').innerHTML =
+            `Ai răspuns corect la ${contorCorect} întrebări din 10.`;
+
         document.querySelector('#true').style.display = 'none';
         document.querySelector('#false').style.display = 'none';
-    }
 
+        document.querySelector('.restart').innerHTML = `
+            <button class="raspuns mt-2 w-[300px] h-[50px]" id="restart-btn">
+                <img class="w-[300px] h-[50px] object-cover rounded-[20px]"
+                     src="../../assets/img/true-false-game/image 11.png" alt="">
+            </button>
+        `;
+
+        document.querySelector('#restart-btn').addEventListener('click', function () {
+            restart();
+        });
+    }
 }
+
 
 function dacaECorect(raspunsUtilizator) {
     {
@@ -125,10 +228,22 @@ document.querySelector('#false').addEventListener('click', function () {
     genereazaHTML();
 });
 
+document.querySelector('.restart').addEventListener('click', function () {
+    restart();
+    genereazaHTML();
+});
 
-
-function initializare(){
+function initializare() {
     genereazaHTML()
+}
+
+function restart() {
+    contor = 0;
+    contorCorect = 0;
+    newArr = []
+    arrCuExercitii = [];
+    creareArr();
+    genereazaHTML();
 }
 
 initializare();
