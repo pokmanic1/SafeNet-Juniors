869bda8f52bab232cfed10ba5701c8c3c7a22d12
pokmanic1 <fanulluimadalin13@gmail.com>
2026-03-04 15:20:34 +0300

    in shufle game adaugarea nivelului

--- public/assets/js/modules/shuffle.js
+++ public/assets/js/modules/shuffle.js
@@ -7,34 +7,14 @@ onAuthStateChanged(auth, (user) => {
 });
 
 
-if(JSON.parse(localStorage.getItem("shuffleGameData"))){
-console.log('estele')
-}else{
-    console.log("nu")
-}
-let dateSalvate=JSON.parse(localStorage.getItem("shuffleGameData")) || [];
-console.log("-----------------------------------------");
+
+let dateSalvate = JSON.parse(localStorage.getItem("shuffleGameData")) || [];
+console.log("Datele salvate de profesor -----------------------------------------");
 console.log(dateSalvate);
-console.log("-----------------------------------------");
-console.log(dateSalvate.lenght);
-console.log("-----------------------------------------");
 
-let arrText = [
-    'frauda',
-    'mesaj',
-    'parola',
-    'phishing',
-    'protectie',
-    'riscuri',
-    'securitate',
-    'sigur',
-    'siguranta',
-    'spam',
-    'sterge',
-    'virus',
-];
 
-let arrImaginea = [
+let arrText1 = ['frauda', 'mesaj', 'parola', 'phishing', 'protectie', 'riscuri', 'securitate', 'sigur', 'siguranta', 'spam', 'sterge', 'virus'];
+let arrImaginea1 = [
     '../../../assets/img/img-shufle-game/frauda.png',
     '../../../assets/img/img-shufle-game/mesaj.png',
     '../../../assets/img/img-shufle-game/parola.png',
@@ -48,7 +28,86 @@ let arrImaginea = [
     '../../../assets/img/img-shufle-game/sterge.png',
     '../../../assets/img/img-shufle-game/virus.png',
 ];
-let interval;
+
+let arrText2 = ['frauda2', 'mesaj2', 'parola2', 'phishing2', 'protectie2', 'riscuri2', 'securitate2', 'sigur2', 'siguranta2', 'spam2', 'sterge2', 'virus2'];
+let arrImaginea2 = [
+    '../../../assets/img/img-shufle-game/frauda.png',
+    '../../../assets/img/img-shufle-game/mesaj.png',
+    '../../../assets/img/img-shufle-game/parola.png',
+    '../../../assets/img/img-shufle-game/phishing.png',
+    '../../../assets/img/img-shufle-game/protectie.png',
+    '../../../assets/img/img-shufle-game/riscuri.png',
+    '../../../assets/img/img-shufle-game/securitate.png',
+    '../../../assets/img/img-shufle-game/sigur.png',
+    '../../../assets/img/img-shufle-game/siguranta.png',
+    '../../../assets/img/img-shufle-game/spam.png',
+    '../../../assets/img/img-shufle-game/sterge.png',
+    '../../../assets/img/img-shufle-game/virus.png',
+];
+
+let arrText3 = ['frauda3', 'mesaj3', 'parola3', 'phishing3', 'protectie3', 'riscuri3', 'securitate3', 'sigur3', 'siguranta3', 'spam3', 'sterge3', 'virus3'];
+let arrImaginea3 = [
+    '../../../assets/img/img-shufle-game/frauda.png',
+    '../../../assets/img/img-shufle-game/mesaj.png',
+    '../../../assets/img/img-shufle-game/parola.png',
+    '../../../assets/img/img-shufle-game/phishing.png',
+    '../../../assets/img/img-shufle-game/protectie.png',
+    '../../../assets/img/img-shufle-game/riscuri.png',
+    '../../../assets/img/img-shufle-game/securitate.png',
+    '../../../assets/img/img-shufle-game/sigur.png',
+    '../../../assets/img/img-shufle-game/siguranta.png',
+    '../../../assets/img/img-shufle-game/spam.png',
+    '../../../assets/img/img-shufle-game/sterge.png',
+    '../../../assets/img/img-shufle-game/virus.png',
+];
+
+let arrText4 = ['frauda4', 'mesaj4', 'parola4', 'phishing4', 'protectie4', 'riscuri4', 'securitate4', 'sigur4', 'siguranta4', 'spam4', 'sterge4', 'virus4'];
+let arrImaginea4 = [
+    '../../../assets/img/img-shufle-game/frauda.png',
+    '../../../assets/img/img-shufle-game/mesaj.png',
+    '../../../assets/img/img-shufle-game/parola.png',
+    '../../../assets/img/img-shufle-game/phishing.png',
+    '../../../assets/img/img-shufle-game/protectie.png',
+    '../../../assets/img/img-shufle-game/riscuri.png',
+    '../../../assets/img/img-shufle-game/securitate.png',
+    '../../../assets/img/img-shufle-game/sigur.png',
+    '../../../assets/img/img-shufle-game/siguranta.png',
+    '../../../assets/img/img-shufle-game/spam.png',
+    '../../../assets/img/img-shufle-game/sterge.png',
+    '../../../assets/img/img-shufle-game/virus.png',
+];
+
+
+let arrText = [...arrText1]; 
+let arrImaginea = [...arrImaginea1];
+let nivelul = 1;
+const selectNivel = document.querySelector(".nivelul");
+selectNivel.addEventListener('change', (event) => {
+    nivelul = event.target.value;
+        if (nivelul == 1) {
+        arrText = arrText1;
+        arrImaginea = arrImaginea1;
+    } else if (nivelul == 2) {
+        arrText = arrText2;
+        arrImaginea = arrImaginea2;
+    } else if (nivelul == 3) {
+        arrText = arrText3;
+        arrImaginea = arrImaginea3;
+    } else if (nivelul == 4) {
+        arrText = arrText4;
+        arrImaginea = arrImaginea4;
+    }
+
+    jocul.innerHTML = ''; 
+    newArr = [];          
+    contor = 0;
+    contorPerechi = 0;
+    document.querySelector(".scor").innerHTML = `Scorul : 0`;
+    
+    clearInterval(interval); 
+    
+    initializare(); 
+});
 
 
 let primaIncercareId = null;
@@ -56,38 +115,41 @@ let aDouaIncercareId = null;
 let contor = 0;
 const DELAY = 1000;
 let jocul = document.querySelector('.joc');
-
+let interval;
 let newArr = [];
 
+
+
 function genereazaPerechi() {
-    if(dateSalvate.length===0){
-    let indexuri = [];
-
-    while (indexuri.length < 8) {
-        let rand = Math.floor(Math.random() * arrText.length);
-        if (!indexuri.includes(rand)) {
-            indexuri.push(rand);
+    if (dateSalvate.length === 0) {
+        let indexuri = [];
+        // 
+        while (indexuri.length < 8) {
+            let rand = Math.floor(Math.random() * arrText.length);
+            if (!indexuri.includes(rand)) {
+                indexuri.push(rand);
+            }
         }
+
+        indexuri.forEach(i => {
+
+            newArr.push({
+                id: i,
+                type: "text",
+                value: arrText[i]
+            });
+
+            newArr.push({
+                id: i,
+                type: "image",
+                value: arrText[i],
+                imaginea: arrImaginea[i]
+            });
+
+        });
     }
-
-    indexuri.forEach(i => {
-
-        newArr.push({
-            id: i,             
-            type: "text",
-            value: arrText[i]
-        });
-
-        newArr.push({
-            id: i,              
-            type: "image",
-            value: arrText[i],
-            imaginea: arrImaginea[i]
-        });
-
-    });}
-    else{
-        newArr=[...dateSalvate];
+    else {
+        newArr = [...dateSalvate];
     }
 }
 
@@ -96,12 +158,11 @@ function shuffle(array) {
     return array.sort(() => Math.random() - 0.5);
 }
 
-
 function createCard(item) {
-    
+
     const card = document.createElement('div');
     card.classList.add('card');
-   card.dataset.id = Number(item.id);
+    card.dataset.id = Number(item.id);
 
     const front = document.createElement('div');
     front.classList.add('front');
@@ -128,16 +189,17 @@ function createCard(item) {
     return card;
 }
 
-
 function initializare() {
     genereazaPerechi();
     shuffle(newArr);
-    pornesteCeas(0,0);
+    pornesteCeas(0, 0);
     newArr.forEach(item => {
         const card = createCard(item);
         jocul.appendChild(card);
     });
+
 }
+
 function Click(event) {
     const clicked = event.target.closest('.card');
     if (!clicked || clicked.classList.contains('matched') || clicked.classList.contains('selected')) return;
@@ -162,26 +224,27 @@ function match() {
     const selected = document.querySelectorAll('.selected');
     selected.forEach(card => {
         card.classList.add('matched');
-        document.querySelector(".scor").innerHTML=`${contorPerechi++}`;
+        document.querySelector(".scor").innerHTML = `Scorul : ${contorPerechi++}`;
     });
 }
 
 function reset() {
-   primaIncercareId = null;
-aDouaIncercareId = null;
+    localStorage.removeItem("shuffleGameData");
+    primaIncercareId = null;
+    aDouaIncercareId = null;
     contor = 0;
-
+    
     const selected = document.querySelectorAll('.selected');
     selected.forEach(card => {
         card.classList.remove('selected', 'flipped');
     });
 }
 
-function pornesteCeas(minute , secunde) {
-    
-     interval = setInterval(() => {
-        
-            document.querySelector(".time").innerHTML=`${String(minute).padStart(2, '0')}:${ String(secunde).padStart(2, '0')}`;
+function pornesteCeas(minute, secunde) {
+
+    let interval = setInterval(() => {
+
+        document.querySelector(".time").innerHTML = `${String(minute).padStart(2, '0')}:${String(secunde).padStart(2, '0')}`;
 
 
         secunde++;
@@ -192,24 +255,35 @@ function pornesteCeas(minute , secunde) {
         }
 
         if (minute === 59 && secunde === 59) {
-            console.log("59:59");
+
             clearInterval(interval);
-            console.log("Ceas oprit!");
+
         }
 
     }, 1000);
-    
+
 }
 
-document.querySelector(".restart").addEventListener('click',()=>{
- primaIncercareId = null;
- aDouaIncercareId = null;
- contor = 0;
- jocul.innerHTML='';
+
+
+
+
+
+
+document.querySelector(".restart").addEventListener('click', () => {
+    primaIncercareId = null;
+    aDouaIncercareId = null;
+    newArr = [];
+    contor = 0;
+       contorPerechi = 0;
+    document.querySelector(".scor").innerHTML = `Scorul : ${contorPerechi}`;
+
+    
+    jocul.innerHTML = '';
     clearInterval(interval);
- initializare();
+    initializare();
 });
-let contorPerechi=0;
+let contorPerechi = 0;
 
 jocul.addEventListener('click', Click);
 initializare();
