11e76ce5bf45d6a3119d374e1e1df5571546c4e1
pokmanic1 <fanulluimadalin13@gmail.com>
2026-03-18 11:18:04 +0300

    Adaugare si la restul jocurilor pagina transmisie daca nu e conectat

--- public/assets/js/modules/password-game.js
+++ public/assets/js/modules/password-game.js
@@ -1,7 +1,9 @@
 import { auth, onAuthStateChanged } from "../fierbase/firebase-init.js";
 onAuthStateChanged(auth, (user) => {
     if (!user) {
-        window.location.href = "../../../pages/conecteazate.html";
+        // window.location.href = "../../../pages/conecteazate.html";
+        const DacaNuSaConectat=document.getElementById("dacaNuSaConectat");
+        DacaNuSaConectat.classList.remove("hidden");
     }
 });
 
