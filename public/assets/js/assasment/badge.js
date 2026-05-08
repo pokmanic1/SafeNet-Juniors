e0a870d9c1c6917c350826937d7467952a65c7f5
pokmanic1 <fanulluimadalin13@gmail.com>
2026-05-08 09:40:58 +0300

    pagina bagdge html design si inceputul js

---
+++ public/assets/js/assasment/badge.js
@@ -0,0 +1,302 @@
+let arrBadge = [
+
+    // ---------------- SHUFFLE - COMPLETAT ----------------
+    [
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Bronze',
+            cerinta: 'Completează jocul Shuffle de 5 ori',
+            descriere: 'Ai completat jocul Shuffle de 5 ori',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Silver',
+            cerinta: 'Completează jocul Shuffle de 10 ori',
+            descriere: 'Ai completat jocul Shuffle de 10 ori',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Gold',
+            cerinta: 'Completează jocul Shuffle de 20 de ori',
+            descriere: 'Ai completat jocul Shuffle de 20 de ori',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Diamond',
+            cerinta: 'Completează jocul Shuffle de 30 de ori',
+            descriere: 'Ai completat jocul Shuffle de 30 de ori',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Legend',
+            cerinta: 'Completează jocul Shuffle de 50 de ori',
+            descriere: 'Ai completat jocul Shuffle de 50 de ori',
+        },
+
+        // ---------------- SHUFFLE - CÂȘTIGAT ----------------
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Bronze',
+            cerinta: 'Câștigă jocul Shuffle o dată',
+            descriere: 'Ai câștigat jocul Shuffle o dată',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Silver',
+            cerinta: 'Câștigă jocul Shuffle de 3 ori',
+            descriere: 'Ai câștigat jocul Shuffle de 3 ori',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Gold',
+            cerinta: 'Câștigă jocul Shuffle de 7 ori',
+            descriere: 'Ai câștigat jocul Shuffle de 7 ori',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Diamond',
+            cerinta: 'Câștigă jocul Shuffle de 14 ori',
+            descriere: 'Ai câștigat jocul Shuffle de 14 ori',
+        },
+        {
+            tip: 'Joc Shuffle',
+            rank: 'Legend',
+            cerinta: 'Câștigă jocul Shuffle de 20 de ori',
+            descriere: 'Ai câștigat jocul Shuffle de 20 de ori',
+        },
+    ],
+    // ---------------- TRUE OR FALSE - COMPLETAT ----------------
+    [
+        {
+            tip: 'True sau False',
+            rank: 'Bronze',
+            cerinta: 'Completează jocul True sau False de 5 ori',
+            descriere: 'Ai completat jocul True sau False de 5 ori',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Silver',
+            cerinta: 'Completează jocul True sau False de 10 ori',
+            descriere: 'Ai completat jocul True sau False de 10 ori',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Gold',
+            cerinta: 'Completează jocul True sau False de 20 de ori',
+            descriere: 'Ai completat jocul True sau False de 20 de ori',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Diamond',
+            cerinta: 'Completează jocul True sau False de 30 de ori',
+            descriere: 'Ai completat jocul True sau False de 30 de ori',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Legend',
+            cerinta: 'Completează jocul True sau False de 50 de ori',
+            descriere: 'Ai completat jocul True sau False de 50 de ori',
+        },
+
+        // ---------------- TRUE OR FALSE - CÂȘTIGAT ----------------
+        {
+            tip: 'True sau False',
+            rank: 'Bronze',
+            cerinta: 'Câștigă jocul True sau False o dată',
+            descriere: 'Ai câștigat jocul True sau False o dată',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Silver',
+            cerinta: 'Câștigă jocul True sau False de 3 ori',
+            descriere: 'Ai câștigat jocul True sau False de 3 ori',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Gold',
+            cerinta: 'Câștigă jocul True sau False de 7 ori',
+            descriere: 'Ai câștigat jocul True sau False de 7 ori',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Diamond',
+            cerinta: 'Câștigă jocul True sau False de 14 ori',
+            descriere: 'Ai câștigat jocul True sau False de 14 ori',
+        },
+        {
+            tip: 'True sau False',
+            rank: 'Legend',
+            cerinta: 'Câștigă jocul True sau False de 20 de ori',
+            descriere: 'Ai câștigat jocul True sau False de 20 de ori',
+        },
+
+    ],
+    // ---------------- PASSWORD - COMPLETAT ----------------
+    [
+        {
+            tip: 'Password',
+            rank: 'Bronze',
+            cerinta: 'Completează jocul Password de 5 ori',
+            descriere: 'Ai completat jocul Password de 5 ori',
+        },
+        {
+            tip: 'Password',
+            rank: 'Silver',
+            cerinta: 'Completează jocul Password de 10 ori',
+            descriere: 'Ai completat jocul Password de 10 ori',
+        },
+        {
+            tip: 'Password',
+            rank: 'Gold',
+            cerinta: 'Completează jocul Password de 20 de ori',
+            descriere: 'Ai completat jocul Password de 20 de ori',
+        },
+        {
+            tip: 'Password',
+            rank: 'Diamond',
+            cerinta: 'Completează jocul Password de 30 de ori',
+            descriere: 'Ai completat jocul Password de 30 de ori',
+        },
+        {
+            tip: 'Password',
+            rank: 'Legend',
+            cerinta: 'Completează jocul Password de 50 de ori',
+            descriere: 'Ai completat jocul Password de 50 de ori',
+        },
+
+        // ---------------- PASSWORD - CÂȘTIGAT ----------------
+        {
+            tip: 'Password',
+            rank: 'Bronze',
+            cerinta: 'Câștigă jocul Password o dată',
+            descriere: 'Ai câștigat jocul Password o dată',
+        },
+        {
+            tip: 'Password',
+            rank: 'Silver',
+            cerinta: 'Câștigă jocul Password de 3 ori',
+            descriere: 'Ai câștigat jocul Password de 3 ori',
+        },
+        {
+            tip: 'Password',
+            rank: 'Gold',
+            cerinta: 'Câștigă jocul Password de 7 ori',
+            descriere: 'Ai câștigat jocul Password de 7 ori',
+        },
+        {
+            tip: 'Password',
+            rank: 'Diamond',
+            cerinta: 'Câștigă jocul Password de 14 ori',
+            descriere: 'Ai câștigat jocul Password de 14 ori',
+        },
+        {
+            tip: 'Password',
+            rank: 'Legend',
+            cerinta: 'Câștigă jocul Password de 20 de ori',
+            descriere: 'Ai câștigat jocul Password de 20 de ori',
+        },
+    ],
+    // ---------------- VARIANTE - COMPLETAT ----------------
+    [
+        {
+            tip: 'Variante',
+            rank: 'Bronze',
+            cerinta: 'Completează jocul Variante de 5 ori',
+            descriere: 'Ai completat jocul Variante de 5 ori',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Silver',
+            cerinta: 'Completează jocul Variante de 10 ori',
+            descriere: 'Ai completat jocul Variante de 10 ori',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Gold',
+            cerinta: 'Completează jocul Variante de 20 de ori',
+            descriere: 'Ai completat jocul Variante de 20 de ori',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Diamond',
+            cerinta: 'Completează jocul Variante de 30 de ori',
+            descriere: 'Ai completat jocul Variante de 30 de ori',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Legend',
+            cerinta: 'Completează jocul Variante de 50 de ori',
+            descriere: 'Ai completat jocul Variante de 50 de ori',
+        },
+
+        // ---------------- VARIANTE - CÂȘTIGAT ----------------
+        {
+            tip: 'Variante',
+            rank: 'Bronze',
+            cerinta: 'Câștigă jocul Variante o dată',
+            descriere: 'Ai câștigat jocul Variante o dată',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Silver',
+            cerinta: 'Câștigă jocul Variante de 3 ori',
+            descriere: 'Ai câștigat jocul Variante de 3 ori',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Gold',
+            cerinta: 'Câștigă jocul Variante de 7 ori',
+            descriere: 'Ai câștigat jocul Variante de 7 ori',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Diamond',
+            cerinta: 'Câștigă jocul Variante de 14 ori',
+            descriere: 'Ai câștigat jocul Variante de 14 ori',
+        },
+        {
+            tip: 'Variante',
+            rank: 'Legend',
+            cerinta: 'Câștigă jocul Variante de 20 de ori',
+            descriere: 'Ai câștigat jocul Variante de 20 de ori',
+        },
+    ]
+];
+
+let arrTitluri = ['Joc Shuffle', 'True sau False', 'Password', 'Variante'];
+    console.log(arrBadge[0]);
+ console.log('--------------------------------');
+  console.log('--------------------------------');
+   console.log('--------------------------------');
+ console.log('--------------------------------');
+  console.log('--------------------------------');
+   console.log('--------------------------------');
+
+arrTitluri.forEach((titlu) =>{
+    
+    console.log(titlu);
+
+    arrBadge.forEach((item) =>{
+        console.log(item[i]);
+    });
+    i++;
+});
+
+
+
+ console.log('--------------------------------');
+  console.log('--------------------------------');
+   console.log('--------------------------------');
+   console.log('--------------------------------');
+   console.log('--------------------------------');
+   console.log('--------------------------------');
+console.log(arrBadge[0]);
+console.log(arrBadge[0]);
+
+let badgeHTML = '';
+
+
+
+
+
+
