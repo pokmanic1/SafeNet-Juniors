c13e2bb52de3dec9d83f8661a292d7ad953be965
pokmanic1 <fanulluimadalin13@gmail.com>
2026-05-16 12:32:55 +0300

    blocam paginile de profesori pentru elevi

---
+++ public/assets/js/dashboard/autentficat-profesor.js
@@ -0,0 +1,19 @@
+import { auth, db, onAuthStateChanged } from "../fierbase/firebase-init.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+onAuthStateChanged(auth, async (user) => {
+    console.log("USER:", user);
+    if (!user) return;
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (userSnap.exists()) {
+
+        const data = userSnap.data();
+
+        if (data.role !== "Profesor") {
+                    window.location.href = "/public/index.html";
+        }
+
+    }
+
+});
