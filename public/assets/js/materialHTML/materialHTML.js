0e5757b0296c3524fb7b6b4cc407ee001ff00e25
pokmanic1 <fanulluimadalin13@gmail.com>
2026-05-25 09:42:23 +0300

    modificari

--- public/assets/js/materialHTML/materialHTML.js
+++ public/assets/js/materialHTML/materialHTML.js
@@ -101,7 +101,7 @@ function buildHeader() {
 function buildFooter() {
   const footer = `
           <section
-    class="flex flex-col items-center justify-center w-full bg-white border-t dark:bg-[#1A1C26] subsol dark:border-[#4C5074]">
+    class="flex flex-col items-center justify-center w-full bg-white  dark:bg-[#1A1C26] subsol ">
     <div class="w-[85%] mx-auto px-2 md:px-[100px] py-16">
 
       <div class="flex flex-col gap-12 md:flex-row md:justify-between md:items-start">
